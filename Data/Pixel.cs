2e4b1d8899e807d6bbf8927474cf4b1188d87a7b
Alina <alinasoldatkina2014@gmail.com>
2021-02-07 18:37:44 +0300

    Задание 2

--- Data/Pixel.cs
+++ Data/Pixel.cs
@@ -14,6 +14,7 @@ namespace MyPhotoshop
 
         public Pixel getNewPixelColor(double parameter)
         {
+            parameter = parameter >= 1 ? 1 : parameter;
             return new Pixel(Color.FromArgb((int)(this.color.R * parameter), (int)(this.color.G * parameter), (int)(this.color.B * parameter)));
         }
     }
